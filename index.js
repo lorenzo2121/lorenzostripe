63c8469d1a5520180efa759968f71b9a5627e78d
Lorenzo <cuttittalorenzo0@gmail.com>
2024-10-22 10:38:11 +0200

    Removed sensitive file

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const fs = require('fs');
-const path = require('path');
 const app = express();
 
-// Configura Firebase Admin SDK
-const serviceAccountPath = '/etc/secrets/cianfoni-51cc8-firebase-adminsdk-q8102-516fc87b17.json'; // Path del file segreto
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+// Initialize Firebase Admin SDK
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,16 +14,14 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-// Questo è il segreto del webhook per il test
+// Stripe webhook secret
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-// Middleware per parsare il corpo della richiesta come JSON
-app.use(express.json());
+// General middleware
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 
-// Endpoint per creare una sessione di checkout
-app.post('/create-checkout-session', async (req, res) => {
+// Endpoint to create a checkout session
+app.post('/create-checkout-session', express.json(), async (req, res) => {
   const { priceId, userEmail } = req.body;
 
   try {
@@ -41,7 +36,7 @@ app.post('/create-checkout-session', async (req, res) => {
       success_url: `http://localhost:4242/success?user_email=${userEmail}`,
       cancel_url: 'http://localhost:4242/canceled',
     });
-
+    
     res.json({ url: session.url });
   } catch (error) {
     console.error('Error creating checkout session:', error);
@@ -49,24 +44,16 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
-// Gestisci gli eventi del webhook di Stripe
-app.post('/webhook', express.raw({ type: 'application/json' }), async (request, response) => {
-  const sig = request.headers['stripe-signature'];
-  
-  let event;
-
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    console.error(`Webhook error: ${err.message}`);
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
-  }
+// Stripe webhook endpoint
+app.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
+  console.log("webhook")
+  const bodyString = req.body.toString('utf8');
+  const event = JSON.parse(bodyString); // Parse the JSON string into an object
 
   switch (event.type) {
     case 'checkout.session.completed':
       const session = event.data.object;
-
+      // Extract user email from success_url
       const urlParams = new URLSearchParams(new URL(session.success_url).search);
       const userEmail = urlParams.get('user_email');
 
@@ -84,7 +71,6 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (request, 
           });
           console.log(`Updated user ${doc.id} with 10 additional requests.`);
         });
-
       } catch (error) {
         console.error(`Error updating user with email ${userEmail}:`, error);
       }
@@ -93,8 +79,8 @@ app.post('/webhook', express.raw({ type: 'application/json' }), async (request, 
       console.log(`Unhandled event type ${event.type}`);
   }
 
-  response.json({ received: true });
+  res.json({ received: true });
 });
 
-// Avvia il server sulla porta 4242
+// Start server on port 4242
 app.listen(4242, () => console.log('Server is running on port 4242'));
